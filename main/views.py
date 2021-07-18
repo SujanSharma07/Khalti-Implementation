231b4dee0a2561b3f6c266a6b1490ef017f6f4ee
SujanSharma07 <sujansahrma202@gmail.com>
2021-07-18 17:45:35 +0545

    First

---
+++ main/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+import requests
+from django.http import JsonResponse
+# Create your views here.
+
+
+def home(request):
+    try:
+        data = request.GET
+        token = data['token']
+        amount = data['amount']
+   
+        url = "https://khalti.com/api/v2/payment/verify/"
+        params = {
+        "token": token,
+        "amount": amount
+        }
+        headers = {
+        "Authorization": "Key test_secret_key_f85495d44530418491c4cf92d1cccac0"
+        }
+
+        response = requests.post(url, params, headers = headers)
+        if response.status_code == 200:
+            return JsonResponse({'data':amount,'status':"Success"})
+
+        else:
+            return JsonResponse({'data':amount,'status':"Error"})
+    except:
+        pass
+    return render(request,'home.html')
+
+
